6088bd01e20782f87a973b24c4ccf1010d498c80
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-08-25 19:13:43 +0200

    Added Lesson 2

---
+++ Lektion-02/atm/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace atm;
+
+class Program
+{
+    static void Main()
+    {
+        // Deklarera en variabel för aktuellt saldo...
+        int balance;
+        // Insättning...
+        Console.WriteLine("Hur mycket vill du sätta in?");
+        // Deklarera en variabel för insättningen...
+        var amount = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(amount))
+        {
+            Console.WriteLine("DU MÅSTE ANGE HUR MYCKET DU VILL SÄTTA IN!!!");
+            Environment.Exit(0);
+        }
+
+        balance = int.Parse(amount);
+
+        Console.WriteLine($"Du har {balance} på ditt konto");
+
+        Console.WriteLine("Hur mycket vill du ta ut?");
+        amount = Console.ReadLine();
+        Console.WriteLine($"Du vill ta ut {amount} Kr");
+
+        balance -= int.Parse(amount);
+        // balance = balance - int.Parse(amount);
+
+        Console.WriteLine($"Du har {balance} på ditt konto");
+    }
+}
---
+++ Lektion-02/intro/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace intro;
+
+class Program
+{
+    static void Main()
+    {
+        // explicit...
+        string name = "Nisse";
+        Console.WriteLine(name);
+
+        // implicit...
+        var lastName = "Nilsson";
+        Console.WriteLine(name + " " + lastName);
+
+        var age = 25;
+        // Inte snyggt...
+        Console.WriteLine(name + " " + lastName + " " + age);
+        // String interpolation via string templates...
+        Console.WriteLine($"Ditt namn är {name} {lastName} du är {age} gammal");
+
+        var result = Calc(10);
+        Console.WriteLine($"Svaret är {result}");
+
+        var result2 = Calc(3.75M);
+        Console.WriteLine($"Svaret är {result2}");
+
+        var result3 = Calc(1,2,3,4,5,6,7,8,9,10);
+        Console.WriteLine(result3);
+    }
+
+    static void Test()
+    {
+        // name ="Kalle";
+    }
+
+    static int Calc(int value)
+    {
+        return value + 10;
+    }
+
+    static decimal Calc(decimal value)
+    {
+        return value;
+    }
+
+    static int Calc(int val1,int val2)
+    {
+        return val1 + val2;
+    }
+
+    static int Calc(params int[] numbers)
+    {
+        return numbers.Sum();
+    }
+
+    
+}
