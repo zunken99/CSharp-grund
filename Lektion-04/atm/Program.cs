baae92abfc93667e7dd1be60f8d174e8fd5b7d14
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-03 12:02:18 +0200

    Added lesson 4

---
+++ Lektion-04/atm/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace atm;
+
+class Program
+{
+
+    // Deklarera en variabel för aktuellt saldo...
+    static int balance = 0;
+    // Deklarera en variable för transaktioner...
+    static List<string> transactions = new List<string>();
+    static void Main()
+    {
+        // Påbörjar vår meny...
+        Console.WriteLine("------------------------------------------------------------------");
+        Console.WriteLine("Välkommen Westcoast Bank");
+        Console.WriteLine("Meny alternativ");
+        Console.WriteLine("För att avsluta programmet tryck på tangenten 'x'");
+        Console.WriteLine("För att sätta in pengar tryck på tangenent 'd'");
+        Console.WriteLine("För att ta ut pengar tryck på tangenten 'w'");
+        Console.WriteLine("För att visa saldot tryck på tangenten 'b'");
+        Console.WriteLine("För att visa transaktioner tryck på tangenten 't'");
+        Console.WriteLine("------------------------------------------------------------------");
+
+        while (true)
+        {
+            var key = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(key) || key == "x")
+            {
+                Environment.Exit(0);
+            }
+            else if (key == "d")
+            {
+                Console.WriteLine("Hur mycket vill du sätta in?");
+                var amount = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(amount))
+                {
+                    Console.WriteLine("Du måste ange ett heltals belopp som du vill sätta in!");
+                    Environment.Exit(0);
+                }
+
+                Deposit(amount);
+            }
+            else if (key == "w")
+            {
+                Console.WriteLine("Hur mycket vill du ta ut?");
+                var amount = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(amount))
+                {
+                    Console.WriteLine("Du måste ange ett heltals belopp som du vill ta ut!");
+                    Environment.Exit(0);
+                }
+
+                WithDraw(amount);
+            }
+            else if (key == "b")
+            {
+                DisplayBalance();
+            }
+            else if (key == "t")
+            {
+                DisplayTransactions();
+            }
+            else
+            {
+                Console.WriteLine("Jag förstår inte ditt val");
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    // Enkel metod för att skriva ut saldot...
+    static void DisplayBalance()
+    {
+        Console.WriteLine($"Du har {balance} på ditt konto");
+    }
+
+    static void DisplayTransactions()
+    {
+        foreach (var tran in transactions)
+        {
+            Console.WriteLine(tran);
+        }
+    }
+
+    static void Deposit(string amount)
+    {
+        if (int.TryParse(amount, out int result))
+        {
+            balance += result;
+            transactions.Add($"Transaktionsdatum: {DateTime.Now} - Transakationstyp: insättning - Transaktionvärde: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Tyvärr förstår inte jag vad du menar");
+            Environment.Exit(0);
+        }
+    }
+    static void WithDraw(string amount)
+    {
+        if (int.TryParse(amount, out int result))
+        {
+            balance -= result;
+            transactions.Add($"Transaktionsdatum: {DateTime.Now} - Transakationstyp: uttag - Transaktionvärde: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Tyvärr förstår inte jag vad du menar");
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Lektion-04/dynamic-lists/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+
+namespace dynamic_lists;
+
+class Program
+{
+    static void Main()
+    {
+        // =====================================================
+        // Basic lists - ArrayList - Inte rekommenderade längre
+        // =====================================================
+        /*
+        ArrayList demo = new ArrayList();
+        // ArrayList demo = [];
+        demo.Add("Michael");
+        demo.Add(35);
+        demo.Add(false);
+        demo.Add(new { make = "Volvo", model = "XC60", mileage = 1200000, modelYear = 2017 });
+
+        foreach (var item in demo)
+        {
+            Console.WriteLine(item);
+        }
+        */
+
+        // =====================================================
+        // Basic lists - List<T>
+        // =====================================================
+        List<string> movies = new List<string>();
+        // List<string> movies = [];
+        movies.Add("Bucket List");
+        movies.Add("Batman");
+        movies.Add("Die Hard");
+
+        Console.WriteLine($"Antal filmer: {movies.Count}");
+        Console.WriteLine($"Kapacitet: {movies.Capacity}");
+
+        foreach (var movie in movies)
+        {
+            Console.WriteLine(movie);
+        }
+
+        movies.Reverse();
+
+        Console.WriteLine("");
+        Console.WriteLine("Reversed order ---------------------------------------------");
+        Console.WriteLine("");
+
+        foreach (var movie in movies)
+        {
+            Console.WriteLine(movie);
+        }
+
+        movies.Sort();
+
+        Console.WriteLine("");
+        Console.WriteLine("Sorted order ---------------------------------------------");
+        Console.WriteLine("");
+
+        foreach (var movie in movies)
+        {
+            Console.WriteLine(movie);
+        }
+
+        // =====================================================
+        // Basic lists - Queue<T>
+        // =====================================================
+
+        /*
+        Console.WriteLine("");
+        Console.WriteLine("Queue ---------------------------------------------");
+        Console.WriteLine("");
+
+        Queue<string> comics = new Queue<string>();
+        // Queue<string> comics = [];
+        comics.Enqueue("Buster");
+        comics.Enqueue("Läderlappen");
+        comics.Enqueue("X9");
+
+        foreach (var comic in comics)
+        {
+            Console.WriteLine(comic);
+        }
+
+        var newComics = comics.Shuffle();
+        comics.Dequeue();
+        Console.WriteLine("");
+
+        foreach (var comic in comics)
+        {
+            Console.WriteLine(comic);
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Shuffled Queue ---------------------------------------------");
+        Console.WriteLine("");
+
+        foreach (var comic in newComics)
+        {
+            Console.WriteLine(comic);
+        }
+        */
+
+        Console.WriteLine("");
+        Console.WriteLine("Stack ---------------------------------------------");
+        Console.WriteLine("");
+
+        Stack<int> test = new Stack<int>();
+        // Stack<int> test = [];
+        test.Push(1);
+        test.Push(2);
+        test.Push(3);
+        test.Push(4);
+        test.Push(5);
+
+        foreach (var item in test)
+        {
+            Console.WriteLine(item);
+        }
+
+        test.Pop();
+
+        Console.WriteLine("");
+        Console.WriteLine("Stack efter pop() ---------------------------------------------");
+        Console.WriteLine("");
+
+        foreach (var item in test)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
