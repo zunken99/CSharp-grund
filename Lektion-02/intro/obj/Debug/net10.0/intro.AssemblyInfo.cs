172b2b47118af845ed1979d67f909fc7789503aa
zunken99 <isakoaderu@gmail.com>
2026-09-17 12:40:22 +0200

    commit

--- Lektion-02/intro/obj/Debug/net10.0/intro.AssemblyInfo.cs
+++ Lektion-02/intro/obj/Debug/net10.0/intro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6853ae07c1b4de5059bb2224714304d0d4d649e")]
 [assembly: System.Reflection.AssemblyProductAttribute("intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
